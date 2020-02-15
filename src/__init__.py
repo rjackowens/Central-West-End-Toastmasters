a49a19a1d3ae89d9b0c572f49eb06fda539af3f1
Jack Owens <40874726+rjackowens@users.noreply.github.com>
2020-02-15 16:41:31 -0600

    Fixed Docker breaking change

--- src/__init__.py
+++ src/__init__.py
@@ -4,5 +4,4 @@ app = Flask(__name__) # template_folder="static/templates"
 
 from src import routes
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+app.run(host="0.0.0.0", port=5000)
